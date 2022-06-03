caa7767a135c5cf2d6fa6cf126785ef45ca8d1a6
Meriç Bulca <mericmert@sabanciuniv.edu>
2022-06-03 05:03:34 +0300

    bug fix

--- database/connection.php
+++ database/connection.php
@@ -1,8 +1,8 @@
 <?php
-    $hostname = "localhost";
-    $username =  "root";
-    $password = "meroero2323";
-    $db = "main";
+    $hostname = "eu-cdbr-west-02.cleardb.net";
+    $username =  "b5316bec4f3150";
+    $password = "fa509c1f";
+    $db = "heroku_8a9fd1da9e974b1";
 
     $dbconnect = mysqli_connect($hostname,$username,$password,$db);
     if($dbconnect->connect_error){
